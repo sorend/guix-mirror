a8c9e075c6a6ae44fd2cf0f5da637ea8b042218a
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-04 21:56:17 +0100

    gnu: cogl: Fix build with gcc-14.
    * gnu/packages/gnome.scm (cogl)[arguments]: Add CFLAGS to #:configure-flags to
    relax gcc-14's strictness.
    
    Change-Id: I05c4923c6e2ed60b892d3beef2967fff18cdeb56

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -6338,7 +6338,10 @@ throughout GNOME for API documentation).")
            wayland))
     (arguments
      `(#:disallowed-references (,xorg-server-for-tests)
-       #:configure-flags (list "--enable-cogl-gst"
+       #:configure-flags (list ,(string-append
+                                 "CFLAGS=-g -O2"
+                                 " -Wno-error=implicit-function-declaration")
+                               "--enable-cogl-gst"
                                "--enable-wayland-egl-platform"
                                "--enable-wayland-egl-server"
 
