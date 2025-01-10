34f7a1ac3de4c1a860ccd78d7317eab1548762dc
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-01-10 12:03:18 +0800

    gnu: krdc: Update to 24.12.1.
    * gnu/packages/kde-internet.scm (krdc): Update to 24.12.1.
    [inputs]: Add kcrash.
    
    Change-Id: I272ac74414f67023eeb385c8b601b03bac2c1e9a

--- gnu/packages/kde-internet.scm
+++ gnu/packages/kde-internet.scm
@@ -378,14 +378,14 @@ This package is part of the KDE networking module.")
 (define-public krdc
   (package
     (name "krdc")
-    (version "24.05.2")
+    (version "24.12.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/krdc-" version ".tar.xz"))
        (sha256
-        (base32 "0xw9ks43llm4lnnswv1h81gmr8mr6191bblmd21c1a5a6vvr935h"))))
+        (base32 "0kwsnmvnqyaj53njpd0424fsd7pkdcv5h162dym2binkq710mdvf"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules pkg-config kdoctools))
@@ -396,6 +396,7 @@ This package is part of the KDE networking module.")
            kcmutils
            kcompletion
            kconfig
+           kcrash
            kdnssd
            ki18n
            kio
