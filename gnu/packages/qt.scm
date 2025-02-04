83064159fb65b6f7a86c276b0868273c5b92b60d
宋文武 <iyzsong@member.fsf.org>
2025-02-04 14:55:30 +0800

    gnu: qtmultimedia: Update to 6.8.2.
    * gnu/packages/qt.scm (qtmultimedia): Update to 6.8.2.
    
    Change-Id: Ie149a2a3d3be05bdd1ee13b47a861808df77b465

--- gnu/packages/qt.scm
+++ gnu/packages/qt.scm
@@ -1958,13 +1958,13 @@ Vulkan, OpenGL and other main graphic APIs.")
 (define-public qtmultimedia
   (package
     (name "qtmultimedia")
-    (version "6.7.2")
+    (version "6.8.2")
     (source (origin
               (method url-fetch)
               (uri (qt-url name version))
               (sha256
                (base32
-                "1lsiarvag8lr4a1apa466xz56b1znjncy8wz5hyiv6nbb88kby4f"))
+                "0s8mxd7pwm9v8x5qa3h6124w91k0zjbbah6h9b68n5bvq3yn3x9l"))
               (modules '((guix build utils)))
               (snippet
                '(begin
