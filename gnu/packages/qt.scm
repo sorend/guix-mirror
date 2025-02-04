b4a030778543d1da4bb6082ca8c12bc0bcbb6921
宋文武 <iyzsong@member.fsf.org>
2025-02-04 14:55:30 +0800

    gnu: qtwayland: Update to 6.8.2.
    * gnu/packages/qt.scm (qtwayland): Update to 6.8.2.
    
    Change-Id: I63bc962509acfdf40526dabe39655dadc8eac73a

--- gnu/packages/qt.scm
+++ gnu/packages/qt.scm
@@ -2089,7 +2089,7 @@ compositor libraries.")))
 (define-public qtwayland
   (package
     (name "qtwayland")
-    (version "6.7.2")
+    (version "6.8.2")
     (source
      (origin
        (method url-fetch)
@@ -2097,7 +2097,7 @@ compositor libraries.")))
        (patches (search-patches
                  "qtwayland-6-update-wayland-xml.patch"))
        (sha256
-        (base32 "0nwa59g1wk7fkym837pkw312abjb376gx44rpd5d8jv4vphmg852"))))
+        (base32 "0iwnvjas5vqzi48finff72iqnl5hal48qba64kwjnpr911wiaijy"))))
     (build-system cmake-build-system)
     (arguments
      (list #:configure-flags #~(list "-DQT_BUILD_TESTS=ON")
